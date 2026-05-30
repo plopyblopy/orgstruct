c57a011ee2aa3b2b41ab8d967b9defad74a2317f
PlopyBlopy <theicebreakerbreakstheice@gmail.com>
2026-05-30 23:52:42 +0300

    feat(main): добавленны функции main, app для построения приложения
    1. main - добавляет базовую настройку
    - Получает config.
    - Настраивает logger через slog с вспомогательной функцией ParseLevel.
    - Вызывает функцию app.
    
    2. app - запуск и завершение приложения.
    - Настройка и запуск http сервера.
    - Ожидает сигнал завершения.
    - Завершает с Graceful Shutdown.

--- orgstruct/cmd/main.go
+++ orgstruct/cmd/main.go
@@ -1,5 +1,117 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
+	"github.com/plopyblopy/orgstruct/internal/adapters/rest"
+	"github.com/plopyblopy/orgstruct/internal/adapters/rest/handlers"
+	"github.com/plopyblopy/orgstruct/internal/domain"
+	"github.com/plopyblopy/orgstruct/internal/shared"
+	"github.com/plopyblopy/orgstruct/internal/shared/config"
+)
+
+func main() {
+	// config
+	cfg, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// logger
+	logLevelStr := cfg.LogLevel
+	if logLevelStr == "" {
+		logLevelStr = "info"
+	}
+
+	level, err := shared.ParseLevel(logLevelStr)
+	if err != nil {
+		level = slog.LevelInfo
+	}
+
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: level,
+	}))
+	slog.SetDefault(log) // global logger
+
+	// service
+	err = app(*cfg, log)
+	if err != nil {
+		log.Error("app error", "err", err)
+	}
+	slog.Info("app stopped")
+}
+
+func app(cfg config.Config, log *slog.Logger) error {
+	slog.Info("launching the application...")
+	var wg sync.WaitGroup
+	errChan := make(chan error, 1)
+
+	// router
+	router := rest.NewRouter()
+	handlers.RegisterRoutes(router)
+	handler := router.InitRouter(1)
+
+	// http server
+	srv := rest.NewHttpServer(handler, cfg.HttpConfig, log)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// выполняется по srv.ListenAndServe не завершится в виду err или Shutdown
+		err := func() (err error) {
+			// перехват паники
+			defer func() {
+				if r := recover(); r != nil {
+					err = domain.NewPanicError(fmt.Sprintf("%v", r))
+				}
+
+			}()
+			return srv.ListenAndServe(cfg.HttpConfig)
+		}()
+
+		if err != nil && err != http.ErrServerClosed {
+			errChan <- err
+			return
+		}
+	}()
+
+	// ожидает сиграл остановки приложения и реализует Graceful Shutdown
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	slog.Info("the app is running!")
+
+	select {
+	case err := <-errChan:
+		log.Error("server error", "err", err)
+		return shutdownServer(srv, &wg, 5*time.Second)
+	case <-stopChan:
+		log.Info("shutdown signal received...")
+		return shutdownServer(srv, &wg, 5*time.Second)
+	}
+}
+
+// Graceful Shutdown для http server
+func shutdownServer(srv *rest.Server, wg *sync.WaitGroup, timeout time.Duration) error {
+	slog.Info("shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("forced shutdown", "err", err)
+		return err
+	}
+
+	wg.Wait()
+	slog.Info("http server stopped")
+	return nil
 }
---
+++ orgstruct/internal/shared/logger.go
@@ -0,0 +1,11 @@
+package shared
+
+import (
+	"log/slog"
+)
+
+func ParseLevel(s string) (slog.Level, error) {
+	var l slog.Level
+	err := l.UnmarshalText([]byte(s))
+	return l, err
+}
