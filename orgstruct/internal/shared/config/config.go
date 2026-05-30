3288c897b846d9641aea4e02b020d9979f485cd0
PlopyBlopy <theicebreakerbreakstheice@gmail.com>
2026-05-30 22:11:06 +0300

    feat(config): добавлена структура Config и функция конструктор
    - Добавленная структура Config хранит значения переменных окружения и встраивает структуры rest.HttpConfig, postgres.DbConfig.
    - Добавленная функция NewConfig формирует структуру Config на основе флага запроса. Используя пакет github.com/joho/godotenv загружает переменные окружения из файла, так же пакет github.com/caarlos0/env/v11 устанавливает значения полям структуры по тегу полей.
    - Добавленная функция FindProjectRoot ищет корень приложения

---
+++ orgstruct/internal/shared/config/config.go
@@ -0,0 +1,79 @@
+package config
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+	"github.com/plopyblopy/orgstruct/internal/adapters/postgres"
+	"github.com/plopyblopy/orgstruct/internal/adapters/rest"
+	"github.com/plopyblopy/orgstruct/internal/shared"
+)
+
+// Config тип для хранения загруженный значений из .env файла
+type Config struct {
+	// APP
+	IsProd bool `env:"IS_PROD"`
+
+	// LOG
+	LogLevel string `env:"LOG_LEVEL"`
+
+	// HTTP
+	rest.HttpConfig
+
+	// DB
+	postgres.DbConfig
+}
+
+// Для определения окружения пытается получить значение из флага, если empty то из environments, если empty то IsProd = false
+// Имя файла .env должно соответствовать переданному окружению, example: .env.dev
+func NewConfig() (*Config, error) {
+	c := &Config{}
+
+	envFlag := flag.String("env", "dev", "Environment: dev|prod|development|production")
+
+	flag.Parse()
+
+	if *envFlag != "" {
+		if *envFlag == "prod" || *envFlag == "production" {
+			c.IsProd = true
+		}
+	} else {
+		curenv := os.Getenv("env")
+		if curenv != "" {
+			if curenv == "prod" || curenv == "production" {
+				c.IsProd = true
+			}
+		}
+	}
+
+	root, err := shared.FindProjectRoot()
+	if err != nil {
+		// Если корень не был найден, не считается ошибкой так как приложение может быть бинарным файлом
+		if !errors.Is(err, os.ErrNotExist) {
+			return nil, err
+		}
+		return nil, err
+	} else {
+		envFile := filepath.Join(root, fmt.Sprintf(".env.%s", *envFlag))
+
+		err := godotenv.Load(envFile)
+		if err != nil {
+			// Если используется иная загрузка env например из docker, а не файл в корне
+			if !errors.Is(err, os.ErrNotExist) {
+				return nil, err
+			}
+		}
+	}
+
+	err = env.Parse(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
