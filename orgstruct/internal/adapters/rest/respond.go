f4b64aa63c652ed7c02958a68fa2081b89b69567
PlopyBlopy <theicebreakerbreakstheice@gmail.com>
2026-06-02 08:23:48 +0300

    feat(respond): добавлена функция RespondError для упрощения формирования ошибки

--- orgstruct/internal/adapters/rest/respond.go
+++ orgstruct/internal/adapters/rest/respond.go
@@ -3,6 +3,8 @@ package rest
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/plopyblopy/orgstruct/internal/domain"
 )
 
 // RespondJSON формирует ответ в формате json
@@ -12,6 +14,29 @@ func RespondJSON(w http.ResponseWriter, status int, data any) {
 	json.NewEncoder(w).Encode(data)
 }
 
-func RespondError(w http.ResponseWriter, status int, message string) {
+func RespondError(w http.ResponseWriter, err error) {
+	var status int
+	if sc, ok := err.(domain.HTTPStatusCoder); ok {
+		status = sc.Code()
+	} else {
+		status = http.StatusInternalServerError
+	}
+
+	if mr, ok := err.(domain.MultiRow); ok {
+		if mr.IsRow() {
+			RespondRowError(w, status, err.Error())
+		} else {
+			RespondMultiRowError(w, status, mr.Rows())
+		}
+	} else {
+		RespondRowError(w, status, err.Error())
+	}
+}
+
+func RespondRowError(w http.ResponseWriter, status int, message string) {
 	RespondJSON(w, status, map[string]string{"error": message})
+}
+
+func RespondMultiRowError[T any](w http.ResponseWriter, status int, errs T) {
+	RespondJSON(w, status, map[string]interface{}{"errors": errs})
 }
