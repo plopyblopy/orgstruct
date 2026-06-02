c62de2727d6a14885038e1ccdad4c87bfff6075c
PlopyBlopy <theicebreakerbreakstheice@gmail.com>
2026-06-02 08:26:08 +0300

    feat(usecase): добавлен usecase для эндпоинта POST /departments

--- orgstruct/internal/usecases/usecases.go
+++ orgstruct/internal/usecases/usecases.go
@@ -4,8 +4,18 @@ import (
 	"github.com/plopyblopy/orgstruct/internal/domain"
 )
 
-func PostDepartment() func(name string, parentId *int) (domain.Department, error) {
-	return func(name string, parentId *int) (domain.Department, error) {
-		return domain.Department{}, nil
+func PostDepartment(r domain.DepartamentAdder) func(name string, parentId *int) (*domain.Department, error) {
+	return func(name string, parentId *int) (*domain.Department, error) {
+		dept, err := domain.NewDepartment(name, parentId)
+		if err != nil {
+			return nil, err
+		}
+
+		createdDept, err := r.Add(dept)
+		if err != nil {
+			return nil, err
+		}
+
+		return createdDept, nil
 	}
 }
