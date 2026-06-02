24f759df18d5760f2b2a055ca20816837cfdf6ab
PlopyBlopy <theicebreakerbreakstheice@gmail.com>
2026-06-02 08:22:22 +0300

    feat(routes): добавлена зависимость DepartmentRepository

--- orgstruct/internal/adapters/rest/handlers/routes.go
+++ orgstruct/internal/adapters/rest/handlers/routes.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/plopyblopy/orgstruct/internal/adapters/postgres"
 	"github.com/plopyblopy/orgstruct/internal/usecases"
 )
 
@@ -12,6 +13,9 @@ type RouteRegistrar interface {
 	AddHandler(pattern string, handler http.HandlerFunc)
 }
 
-func RegisterRoutes(r RouteRegistrar) {
-	r.AddHandler("POST /departments", PostDepartment(usecases.PostDepartment()))
+// RegisterRoutes регистрирует маршруты, создает зависимости.
+func RegisterRoutes(r RouteRegistrar, db *postgres.Db) {
+	departamentRepo := postgres.NewDepartmentRepository(db)
+
+	r.AddHandler("POST /departments", PostDepartment(usecases.PostDepartment(departamentRepo)))
 }
